5196c2dbb053fb5a6d03e8bceb562622b5157b63
David Loveday <davemech@yahoo.com>
2019-03-25 18:29:02 -0700

    refactor with redux:
    implement Login component

--- gif-search/src/reducers/index.js
+++ gif-search/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import GifReducer from './gifs';
 import ModalReducer from './modal';
 import { routerReducer } from 'react-router-redux';
+import { reducer as FormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
     gifs: GifReducer,
     modal: ModalReducer,
-    router: routerReducer
+    router: routerReducer,
+    form: FormReducer,
 });
 
 export default rootReducer;
