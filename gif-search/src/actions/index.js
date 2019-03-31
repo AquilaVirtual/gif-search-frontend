1c16f89bf0315e3a04af7469b84b6680d25281ae
David Loveday <davemech@yahoo.com>
2019-03-30 21:27:21 -0700

    refactor with redux:
    implement action creators for login status

--- gif-search/src/actions/index.js
+++ gif-search/src/actions/index.js
@@ -35,3 +35,23 @@ export function closeModal() {
 }
 
 
+export function setLoginPending(isLoginPending) {
+  return {
+    type: SET_LOGIN_PENDING,
+    isLoginPending
+  };
+}
+
+export function setLoginSuccess(isLoginSuccess) {
+  return {
+    type: SET_LOGIN_SUCCESS,
+    isLoginSuccess
+  };
+}
+
+export function setLoginError(loginError) {
+  return {
+    type: SET_LOGIN_ERROR,
+    loginError
+  }
+}
