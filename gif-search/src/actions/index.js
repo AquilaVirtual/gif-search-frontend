5d8cc6d017c26a1a7f0ef2dd7699e00b27cc09ff
David Loveday <davemech@yahoo.com>
2019-04-03 23:35:52 -0700

    refactor with redux:
    fix bug

--- gif-search/src/actions/index.js
+++ gif-search/src/actions/index.js
@@ -67,18 +67,18 @@ function callLoginApi(email, password, callback) {
 
 export function login(email, password) {
   console.log("We are getting something:", email, password)
-  // return dispatch => {
-  //   dispatch(setLoginPending(true));
-  //   dispatch(setLoginSuccess(false));
-  //   dispatch(setLoginError(null));
-  //  //
-  //   callLoginApi(email, password, error => {
-  //     dispatch(setLoginPending(false));
-  //     if (!error) {
-  //       dispatch(setLoginSuccess(true));
-  //     } else {
-  //       dispatch(setLoginError(error));
-  //     }
-  //   });
-  // }
+  return dispatch => {
+    dispatch(setLoginPending(true));
+    dispatch(setLoginSuccess(false));
+    dispatch(setLoginError(null));
+   //
+    callLoginApi(email, password, error => {
+      dispatch(setLoginPending(false));
+      if (!error) {
+        dispatch(setLoginSuccess(true));
+      } else {
+        dispatch(setLoginError(error));
+      }
+    });
+  }
 }
