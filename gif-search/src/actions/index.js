814f8fbeab77d157d9f47612d30246db8a2a6dcf
David Loveday <davemech@yahoo.com>
2019-04-04 19:44:31 -0700

    refactor with redux:
    fix bug

--- gif-search/src/actions/index.js
+++ gif-search/src/actions/index.js
@@ -65,20 +65,23 @@ function callLoginApi(email, password, callback) {
   }, 1000);
 }
 
+export function signup(name, email, password) {
+  console.log("Sign up called")
+}
 export function login(email, password) {
   console.log("We are getting something:", email, password)
-  return dispatch => {
-    dispatch(setLoginPending(true));
-    dispatch(setLoginSuccess(false));
-    dispatch(setLoginError(null));
-   //
-    callLoginApi(email, password, error => {
-      dispatch(setLoginPending(false));
-      if (!error) {
-        dispatch(setLoginSuccess(true));
-      } else {
-        dispatch(setLoginError(error));
-      }
-    });
-  }
+  // return dispatch => {
+  //   dispatch(setLoginPending(true));
+  //   dispatch(setLoginSuccess(false));
+  //   dispatch(setLoginError(null));
+  //  //
+  //   callLoginApi(email, password, error => {
+  //     dispatch(setLoginPending(false));
+  //     if (!error) {
+  //       dispatch(setLoginSuccess(true));
+  //     } else {
+  //       dispatch(setLoginError(error));
+  //     }
+  //   });
+  // }
 }
--- gif-search/src/containers/SignupForm.js
+++ gif-search/src/containers/SignupForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { signup } from "../actions";
 
 const validate = values => {
   const errors = {};
